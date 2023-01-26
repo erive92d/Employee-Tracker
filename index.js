a98eb916fbd0aa049b41ab575e76e0db99bb206c
Deorren Erive <erive92d@gmail.com>
2023-01-26 11:24:36 -0800

    added new role

--- index.js
+++ index.js
@@ -35,84 +35,35 @@ class View {
   }
 }
 
-// class Manage {
-//   constructor(item) {
-//     this.item = item;
-//   }
-//   addEmployee() {
-//     inquirer
-//       .prompt([
-//         {
-//           type: "input",
-//           name: "firstname",
-//           message: "Enter first name",
-//         },
-//         {
-//           type: "input",
-//           name: "lastname",
-//           message: "Enter last name",
-//         },
-//         {
-//           type: "list",
-//           name: "role",
-//           message: "Enter role",
-//           choices: ["Salesperson", "Software Engineer", "Accountant", "Lawyer"],
-//         },
-//         {
-//           type: "input",
-//           name: "roleid",
-//           message: "Enter role ID",
-//         },
-//         {
-//           type: "input",
-//           name: "managerid",
-//           message: "Enter Manager ID",
-//         },
-//       ])
-//       .then((answer) => {
-//         const { roleid, firstname, lastname, role, managerid } = answer;
-//         let sql = `INSERT INTO employee(role_id,first_name,last_name,role,manager_id)
-//         VALUES (${roleid},"${firstname}","${lastname}","${role}",${managerid});`;
-//         db.query(sql, (err, result) => {
-//           if (err) {
-//             console.log(err);
-//           } else {
-//             console.table(result);
-//           }
-//         });
-//       });
-//   }
-// }
-
 const addEmployee = () => {
   inquirer
     .prompt([
       {
         type: "input",
         name: "firstname",
-        message: "Enter first name"
+        message: "Enter first name",
       },
       {
         type: "input",
         name: "lastname",
-        message: "Enter last name"
+        message: "Enter last name",
       },
       {
         type: "list",
         name: "role",
         message: "Enter role",
-        choices: ["Salesperson", "Software Engineer", "Accountant", "Lawyer"]
+        choices: ["Salesperson", "Software Engineer", "Accountant", "Lawyer"],
       },
       {
         type: "input",
         name: "roleid",
-        message: "Enter role ID"
+        message: "Enter role ID",
       },
       {
         type: "input",
         name: "managerid",
-        message: "Enter Manager ID"
-      }
+        message: "Enter Manager ID",
+      },
     ])
     .then((answer) => {
       const { roleid, firstname, lastname, role, managerid } = answer;
@@ -124,11 +75,70 @@ const addEmployee = () => {
           console.log(err);
         } else {
           console.table(result);
-          init()
+          init();
         }
       });
     });
 };
+
+
+const addDepartment = () => {
+  inquirer.prompt([
+    {
+      type:'input',
+      name:'department',
+      message:'Enter Department name'
+    }
+  ])
+  .then((answer)=> {
+    let sql = `INSERT INTO department (name) 
+    VALUES ("${answer.department}")
+    `
+    db.query(sql,(err,result)=> {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log("Successfully added ",answer.department)
+        init()
+      }
+    })
+  })
+}
+
+const addRole = () => {
+  inquirer.prompt([
+    {
+      type:'input',
+      name:'role',
+      message:'Enter role'
+    },
+    {
+      type:'input',
+      name:'rolebelong',
+      message:'Which department ID this role belongs to?',
+      choices:[1,2,3,4,5]
+    },
+    {
+      type:'input',
+      name:'salary',
+      message:'Enter the salary'
+    }
+  ])
+  .then((answer)=> {
+    const {role,rolebelong,salary} = answer
+    let sql = `INSERT INTO roles (department_id,title,salary)
+    VALUES (${rolebelong},"${role}","${salary}")`
+    db.query(sql,(err,result)=> {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log("Successfully added", role)
+        init()
+      }
+    })
+  })
+}
+
 
 const init = () => {
   inquirer
@@ -142,6 +152,7 @@ const init = () => {
           "View All Roles",
           "View All Employees",
           "Add Department",
+          "Add Role",
           "Add an Employee",
           "Update an Employee Role",
         ],
@@ -149,26 +160,49 @@ const init = () => {
     ])
     .then((answer) => {
       let newAnswer = "";
+      let answerId = "";
       switch (answer.options) {
         case "View All Departments":
           answer.options = "department";
           newAnswer = answer.options;
+          answerId = 1;
           break;
         case "View All Roles":
           answer.options = "roles";
           newAnswer = answer.options;
+          answerId = 1;
           break;
         case "View All Employees":
           answer.options = "employee";
           newAnswer = answer.options;
+          answerId = 1;
           break;
         case "Add an Employee":
-          addEmployee();
+          answerId = 2;
+          break;
+        case "Add Department":
+          answerId = 3;
+          break;
+          case "Add Role":
+            answerId = 4;
+            break;
+        default:
+          console.log("test");
           break;
       }
 
-      const viewDepartment = new View(newAnswer);
-      viewDepartment.viewItem();
+      if (answerId === 1) {
+        const viewDepartment = new View(newAnswer);
+        viewDepartment.viewItem();
+      } else if (answerId === 2) {
+        addEmployee();
+      } else if (answerId === 3) {
+        addDepartment()
+      } else if (answerId === 4) {
+        addRole()
+      }
+
+    
     });
 };
 
